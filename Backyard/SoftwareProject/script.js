4e17d36ff213f6af9be3dceee7bee22ecef450af
UnknownDawn <jbklippst@gmail.com>
2025-11-05 10:13:16 -0500

    Bad Backyard

---
+++ Backyard/SoftwareProject/script.js
@@ -0,0 +1,9 @@
+let numButtonClicks = 0;
+function buttonClicked() {
+    numButtonClicks = numButtonClicks + 1;
+    document.getElementById("mainDiv").textContent =
+        "Button Clicked times: " + numButtonClicks;
+}
+
+
+
